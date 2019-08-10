dea3005f0cc7e3f9a012c9b53ab3d1fbcda9af97
Ammar Altahhan <ammar.tahhan@hungerstation.com>
2019-08-10 13:36:20 +0300

    Initial Commit

---
+++ TransitionalLoader/TransitionalLoader.h
@@ -0,0 +1,19 @@
+//
+//  TransitionalLoader.h
+//  TransitionalLoader
+//
+//  Created by Ammar Altahhan on 10/08/2019.
+//  Copyright © 2019 Ammar Altahhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TransitionalLoader.
+FOUNDATION_EXPORT double TransitionalLoaderVersionNumber;
+
+//! Project version string for TransitionalLoader.
+FOUNDATION_EXPORT const unsigned char TransitionalLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransitionalLoader/PublicHeader.h>
+
+
